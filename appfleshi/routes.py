70961621b85aed4cbde7287c704202e2a257ee54
Donatinnho <lucas.donato@aluno.senai.br>
2025-11-27 15:10:55 -0300

    Feat: add loginform and registerform in pages

---
+++ appfleshi/routes.py
@@ -0,0 +1,32 @@
+from flask import render_template, url_for, redirect
+from flask_login import login_required, login_user, logout_user, current_user
+from appfleshi import app, database, bcrypt
+from appfleshi.forms import LoginForm, RegisterForm
+from appfleshi.models import User, Photo
+
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    login_form = LoginForm()
+    return render_template('homepage.html', form=login_form)
+
+
+@app.route('/createaccount/', methods=['GET', 'POST'])
+def createaccount():
+    register_form = RegisterForm()
+    if register_form.validate_on_submit():
+        password = bcrypt.generate_password_hash(register_form.password.data)
+        user = User(username = register_form.username.data, email = register_form.email.data, password = password)
+        database.session.add(user)
+        database.session.commit()
+        login_user(user, remember=True)
+        return redirect(url_for('profile', username=user.username))
+
+    return render_template('createaccount.html', form=register_form)
+
+
+@app.route('/profile/<username>')
+@login_required
+def profile(username):
+    return render_template('profile.html', username=username)
+
